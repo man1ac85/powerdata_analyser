1f7a7456c1699a7360da1b994c9044f279a61b4d
man1ac85 <166277573+man1ac85@users.noreply.github.com>
2026-05-25 07:32:48 +0200

    Update Dashboard_Online.py

--- Dashboard_Online.py
+++ Dashboard_Online.py
@@ -575,6 +575,6 @@ elif nav_mode == "Daten & Auswertung":
                         fig_hr.update_layout(title="Ø Herzfrequenz", template="plotly_dark")
                         st.plotly_chart(fig_hr, width='stretch')
                     with c3: 
-                    fig3 = px.scatter(df_compare, x="interval_num", y="max_hr", color="Workout", title="Max HF")
-                    fig3.update_traces(mode='lines+markers').update_layout(template="plotly_dark")
-                    st.plotly_chart(fig3, width='stretch')    
+                        fig3 = px.scatter(df_compare, x="interval_num", y="max_hr", color="Workout", title="Max HF")
+                        fig3.update_traces(mode='lines+markers').update_layout(template="plotly_dark")
+                        st.plotly_chart(fig3, width='stretch')    
